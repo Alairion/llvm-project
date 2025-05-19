40288f0f2dafa59172e5fd94d86f26eaa4e2c128
Alexy Pellegrini <alexy.pellegrini@laposte.net>
2025-05-19 19:42:40 +0200

    Add basic intrinsics information

--- llvm/lib/IR/Intrinsics.cpp
+++ llvm/lib/IR/Intrinsics.cpp
@@ -15,6 +15,7 @@
 #include "llvm/ADT/StringTable.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IntrinsicsAArch64.h"
+#include "llvm/IR/IntrinsicsAltairX.h"
 #include "llvm/IR/IntrinsicsAMDGPU.h"
 #include "llvm/IR/IntrinsicsARM.h"
 #include "llvm/IR/IntrinsicsBPF.h"
