a75521ec08accfce3cd629ae57f056b9484205ad
Alexy Pellegrini <alexy.pellegrini@laposte.net>
2025-09-04 00:23:52 +0200

    Add more coherent options to LLD driver for AltairX target

--- clang/lib/Driver/ToolChains/AltairX.cpp
+++ clang/lib/Driver/ToolChains/AltairX.cpp
@@ -28,32 +28,53 @@ void Linker::ConstructJob(Compilation &C, const JobAction &JA,
   bool isLLD{};
   const char *exec = Args.MakeArgString(getToolChain().GetLinkerPath(&isLLD));
 
-  llvm::opt::ArgStringList InputFileList;
-  for(const auto& II : Inputs) {
-    if(!II.isFilename()) {
+  llvm::opt::ArgStringList inputFileList;
+  for(const auto& inputFile : Inputs) {
+    if(!inputFile.isFilename()) {
       // This is a linker input argument.
       // We cannot mix input arguments and file names in a -filelist input, thus
       // we prematurely stop our list (remaining files shall be passed as
       // arguments).
-      if(InputFileList.size() > 0)
+      if(inputFileList.size() > 0) {
         break;
+      }
 
       continue;
     }
 
-    InputFileList.push_back(II.getFilename());
+    inputFileList.push_back(inputFile.getFilename());
   }
 
-  ArgStringList CmdArgs;
-  CmdArgs.push_back("-o");
-  CmdArgs.push_back(Output.getFilename());
+  auto& toolchain = getToolChain();
+  ArgStringList cmdArgs;
+  cmdArgs.push_back("-Bstatic"); // altairx is only a VM for now, only full executables are supported
+  toolchain.AddFilePathLibArgs(Args, cmdArgs);
 
-  Args.AddAllArgs(CmdArgs, options::OPT_L);
-  AddLinkerInputs(getToolChain(), Inputs, Args, CmdArgs, JA);
+  if(!Args.hasArg(options::OPT_nostdlib, options::OPT_nodefaultlibs)) {
+    if(toolchain.ShouldLinkCXXStdlib(Args)) {
+      toolchain.AddCXXStdlibLibArgs(Args, cmdArgs);
+    }
 
-  auto cmd = std::make_unique<Command>(
-    JA, *this, ResponseFileSupport::AtFileUTF8(), exec, CmdArgs, Inputs, Output);
-  cmd->setInputFileList(std::move(InputFileList));
+    if(Args.hasArg(options::OPT_pthread)) {
+      cmdArgs.push_back("-lpthread");
+      cmdArgs.push_back("--shared-memory");
+    }
+
+    cmdArgs.push_back("-lc");
+    //cmdArgs.push_back("-lm");
+    //AddRunTimeLibs(toolchain, toolchain.getDriver(), cmdArgs, Args);
+  }
+
+  cmdArgs.push_back("-o");
+  cmdArgs.push_back(Output.getFilename());
+
+  Args.AddAllArgs(cmdArgs, options::OPT_L);
+  AddLinkerInputs(toolchain, Inputs, Args, cmdArgs, JA);
+
+  auto cmd =
+      std::make_unique<Command>(JA, *this, ResponseFileSupport::AtFileUTF8(),
+                                exec, cmdArgs, Inputs, Output);
+  cmd->setInputFileList(std::move(inputFileList));
   C.addCommand(std::move(cmd));
 }
 
@@ -64,7 +85,13 @@ namespace toolchains {
 
 AltairXToolChain::AltairXToolChain(const Driver &D, const llvm::Triple &Triple,
                                    const ArgList &Args)
-    : ToolChain(D, Triple, Args) {}
+    : ToolChain(D, Triple, Args) {
+  getProgramPaths().push_back(getDriver().Dir);
+
+  auto SysRoot = getDriver().SysRoot;
+  getFilePaths().push_back(SysRoot + "/lib");
+  getLibraryPaths().push_back(SysRoot + "/lib");
+}
 
 bool AltairXToolChain::isPICDefault() const { return false; }
 
